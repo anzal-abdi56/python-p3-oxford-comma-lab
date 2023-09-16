a16fdfc34cf93bddc36dcfaa7948bc3d5debdfe0
+ <+>
2023-09-16 16:45:05 +0300

    Complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 0:
+        return ""
+    elif len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        # Join all elements with commas and add "and" before the last element
+        return f"{', '.join(elements[:-1])}, and {elements[-1]}"
